a1072f29bde1915a29a9f1ae7c739dd1516498c9
seo-png <iseohyeon128@gmail.com>
2022-04-24 16:14:04 +0900

    로그인 페이지 개발

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
+const instance = axios.create({
+	baseURL: process.env.VUE_APP_API_URL,
+});
+
 function registerUser(userData) {
-	const url = 'http://localhost:3000/signup';
-	return axios.post(url, userData);
+	// const url = 'http://localhost:3000/signup';
+	return instance.post('signup', userData);
 }
 
-export { registerUser };
+function loginUser(userData) {
+	return instance.post('login', userData);
+}
+
+export { registerUser, loginUser };
